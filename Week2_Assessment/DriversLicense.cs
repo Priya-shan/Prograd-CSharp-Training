11d2a360b9f52862194d63f02d0784113c9c2908
priya-shan <shanmugapriyashanu2002@gmail.com>
2023-03-31 16:14:15 +0530

    Initial Commit

---
+++ Week2_Assessment/DriversLicense.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_Assessment
+{
+/*
+You have to get a new driver's license. You show up at the office at the same time as four other people. The office says they will see everyone in alphabetical order and it takes 20 minutes for them to process each new license. All of the agents are available now, and they can each see one customer at a time. How long will it take for you to walk out with your new license?
+
+Your input will be a string of your name me, an integer of the number of available agents, and a string of the other four names separated by spaces others.
+
+Return the number of minutes it will take to get your license.
+
+Examples
+License("Eric", 2, "Adam Caroline Rebecca Frank") ➞ 40
+// As you are in the second group of people.
+
+License("Zebediah", 1, "Bob Jim Becky Pat") ➞ 100
+// As you are the last person.
+
+License("Aaron", 3, "Jane Max Olivia Sam") ➞ 20
+// As you are the first.
+*/
+    internal class DriversLicense
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name=Console.ReadLine();
+            Console.WriteLine("ENter number of Agents");
+            int no_of_agents=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ENter other customers name with space separated");
+            string customers = Console.ReadLine();
+
+            string[] otherPeoples = customers.Split(" ");
+
+            //finding eligible peoples i.e, peoples before me in alphabetical order 
+            ArrayList eligiblePeoples=new ArrayList();
+            foreach (string s in otherPeoples)
+            {
+                if((int)s[0] < (int)name[0])
+                {
+                    eligiblePeoples.Add(s);
+                }
+            }
+            //Calculating the Wait time
+            int count_of_eligible = eligiblePeoples.Count;
+            int result = (count_of_eligible / no_of_agents)*20;
+            result = result + 20;
+            Console.WriteLine("Time Taken :"+result);
+        }
+    }
+}
---
+++ Week2_Assessment/EuropeTravel.cs
@@ -0,0 +1,77 @@
+﻿namespace Week2_Assessment
+{
+    /*
+Travelling through Europe one needs to pay attention to how the license plate in the given country is 
+displayed.When crossing the border you need to park on the shoulder,
+unscrew the plate, re-group the characters according to
+the local regulations, attach it back and proceed with the journey.
+
+Create a solution that can format the dmv number
+into a plate number with correct grouping.
+The function input consists of string s and group length n.
+ The output has to be upper case characters and digits.
+The new groups are made from right to left and connected by -.
+ The original order of the dmv number is preserved.
+
+Examples
+LicensePlate("5F3Z-2e-9-w", 4) ➞ "5F3Z-2E9W"
+
+LicensePlate("2-5g-3-J", 2) ➞ "2-5G-3J"
+
+LicensePlate("2-4A0r7-4k", 3) ➞ "24-A0R-74K"
+
+LicensePlate("nlj-206-fv", 3) ➞ "NL-J20-6FV"
+     */
+    internal class EuropeTravel
+    {
+        public static void Main(string[] args)
+        {
+            //Getting User input
+            Console.WriteLine("Enter Your Liscence Plate DMV Number");
+            String DMV_Number = Console.ReadLine();
+            Console.WriteLine("Enter the length acoording to which we have to Group");
+            int group_length=Convert.ToInt32(Console.ReadLine());
+
+            //Removing Dashes from DMV_Number
+            DMV_Number = removeDashes(DMV_Number);
+
+            //Generating LicensePlate
+            string LicensePlate = GenerateLicensePlate(DMV_Number, group_length);
+            Console.WriteLine("Result :"+LicensePlate);
+
+        }
+        public static string removeDashes(String DMV_Number)
+        {
+            string[] arr = DMV_Number.Split("-");
+            string result = "";
+            foreach(String s in arr)
+            {
+                result += s;
+            }
+            return result;
+        }
+        public static string GenerateLicensePlate(String DMV_Number, int group_length)
+        {
+            string LicensePlate = "";
+            int counter = 0;
+            //looping thro string in backwards
+            //change i
+            for(int i= DMV_Number.Length - 1; i >= 0; i--)
+            {
+                if (counter == group_length)
+                {
+                    LicensePlate = "-"+ LicensePlate;
+                    i++;
+                    counter = 0;
+                }
+                else
+                {
+                    LicensePlate = DMV_Number[i] + LicensePlate;
+                    counter++;
+                }
+            }
+            
+            return LicensePlate.ToUpper();
+        }
+    }
+}
