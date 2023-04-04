8bb0680f4f96cd487fc8a6d88209a128cfcea398
priya-shan <shanmugapriyashanu2002@gmail.com>
2023-04-04 19:09:57 +0530

    Code for Poker Combinations

--- Practice_Problems/17_PokerCombination.cs
+++ Practice_Problems/17_PokerCombination.cs
@@ -1,18 +1,13 @@
 ﻿/* 
- In this challenge, you have to establish which kind of Poker combination is present 
-in a deck of five cards. Every card is a string containing the card value 
-(with the upper-case initial for face cards) and the lower-case initial for suits, 
-as in the examples below:
+ In this challenge, you have to establish which kind of Poker combination is present in a deck of five cards. Every card is a string containing the card value (with the upper-case initial for face cards) and the lower-case initial for suits, as in the examples below:
 "Ah" ➞ Ace of hearts
 "Ks" ➞ King of spades
 "3d" ➞ Three of diamonds
 "Qc" ➞ Queen of clubs
 "10c" ➞ Ten of clubs
 
-Given an array hand containing five strings being the cards,
-implement a function that returns a string with the name of the highest combination 
-obtained, accordingly to the table above.
-Examples
+Given an array hand containing five strings being the cards, implement a function that returns a string with the name of the highest combination obtained, accordingly to the table above.
+Examples,
 PokerHandRanking({ "10h", "Jh", "Qh", "Ah", "Kh" }) ➞ "Royal Flush"
 PokerHandRanking({ "3h", "5h", "Qs", "9h", "Ad" }) ➞ "High Card"
 PokerHandRanking({ "10s", "10c", "8d", "10d", "10h" }) ➞ "Four of a Kind"
