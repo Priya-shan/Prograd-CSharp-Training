6df0fb4b92c7f4842f3c875070d247811643ce12
priya-shan <shanmugapriyashanu2002@gmail.com>
2023-04-07 13:00:35 +0530

    initial_commit

---
+++ College_Sports_Management_System/Program.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.Data.SqlClient;
+namespace College_Sports_Management_System
+{
+    internal class Program
+    {
+        public string conn_string = "Data Source=DESKTOP-KN3OCS1;Initial Catalog=College_Sports_Management_System;Integrated Security=True;Encrypt=False";
+        
+        static void Main(string[] args)
+        {
+
+            Program obj= new Program();
+            int opt;
+            Console.WriteLine("\n***********************************************\n" +
+                " Welcome To College Sport Management System" +
+                "\n***********************************************\n" +
+                "\n Press 1 to Add Sport" +
+                "\n Press 2 to Add Tournament" +
+                "\n Press 3 to Add Scoreboard" +
+                "\n Press 4 to Remove Sport" +
+                "\n Press 5 to Remove Tournament" +
+                "\n Press 6 to Edit Scoreboard" +
+                "\n Press 7 to Remove Player" +
+                "\n Press 8 for Individual Registration " +
+                "\n Press 9 for Group Registration" +
+                "\n Press 10 for Payment");
+            do
+            {
+                
+                Console.WriteLine("\n*************\n");
+                opt = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n*************\n");
+
+                switch (opt)
+                {
+                    case 1:
+                        obj.addSport();
+                        break;
+                    case 2:
+                        obj.addTournament();
+                        break;
+
+                    case 3:
+                        obj.addScore();
+                        break;
+
+                    case 4:
+                        obj.remove("Sports","sport_id");
+                        break;
+
+                    case 5:
+                        obj.remove("Tournament", "tournament_id");
+                        break;
+
+                    default:
+                        Console.WriteLine("default");
+                        break;
+                }
+            } while (opt != 0);
+        }
+        public void addSport()
+        {
+            Console.WriteLine("Ok.. ENtering Add SPorts Module");
+            display_table("Sports");
+            SqlConnection conn = new SqlConnection(conn_string);
+            conn.Open();
+            SqlCommand command = conn.CreateCommand();
+
+            Console.WriteLine("\n\nNow enter details to be added in this table ");
+            Console.Write("Enter Sport_Id :");
+            int sport_id = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Sport_Name :");
+            string sport_name=Console.ReadLine();
+
+            string query = "insert into Sports(sport_id,sport_name) Values(@sport_id,@sport_name)";
+            command.Parameters.AddWithValue("@sport_id",sport_id);
+            command.Parameters.AddWithValue("@sport_name", sport_name);
+            command.CommandText=query;
+            command.ExecuteReader();
+
+            conn.Close();
+            Console.WriteLine("\n\nValues Inserted Successfully..!!");
+            display_table("Sports");
+        }
+        public void addTournament()
+        {
+            Console.WriteLine("Ok.. ENtering Add Tournament Module");
+            display_table("Tournament");
+            SqlConnection conn = new SqlConnection(conn_string);
+            conn.Open();
+            SqlCommand command = conn.CreateCommand();
+
+            Console.WriteLine("\n\nNow enter details to be added in this table ");
+            Console.Write("Enter Tournament_id :");
+            int tourn_id = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Tournament_Name :");
+            string tourn_name = Console.ReadLine();
+            Console.Write("Enter Sport_Id :");
+            int sport_id = Convert.ToInt32(Console.ReadLine());
+
+            string query = "insert into Tournament(tournament_id,tournament_name,sport_id) Values(@tournament_id,@tournament_name,@sport_id)";
+            command.Parameters.AddWithValue("@tournament_id", tourn_id);
+            command.Parameters.AddWithValue("@tournament_name", tourn_name);
+            command.Parameters.AddWithValue("@sport_id", sport_id);
+            command.CommandText = query;
+            command.ExecuteReader();
+
+            conn.Close();
+            Console.WriteLine("\n\nValues Inserted Successfully..!!");
+            display_table("Tournament");
+        }
+
+        public void addScore()
+        {
+            Console.WriteLine("Ok.. ENtering Add Scoreboard Module");
+            display_table("Tournament");
+            SqlConnection conn = new SqlConnection(conn_string);
+            conn.Open();
+            SqlCommand command = conn.CreateCommand();
+
+            Console.WriteLine("\n\nNow enter details to be added in this table ");
+            Console.Write("Enter Tournament_id :");
+            int tourn_id = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Tournament_Name :");
+            string tourn_name = Console.ReadLine();
+            Console.Write("Enter Sport_Id :");
+            int sport_id = Convert.ToInt32(Console.ReadLine());
+
+            string query = "insert into Tournament(tournament_id,tournament_name,sport_id) Values(@tournament_id,@tournament_name,@sport_id)";
+            command.Parameters.AddWithValue("@tournament_id", tourn_id);
+            command.Parameters.AddWithValue("@tournament_name", tourn_name);
+            command.Parameters.AddWithValue("@sport_id", sport_id);
+            command.CommandText = query;
+            command.ExecuteReader();
+
+            conn.Close();
+            Console.WriteLine("\n\nValues Inserted Successfully..!!");
+            display_table("Tournament");
+
+        }
+        public void remove(string table_name,string column_name)
+        {
+            Console.WriteLine($"Ok.. ENtering Remove {table_name} Module");
+            display_table(table_name);
+            SqlConnection conn = new SqlConnection(conn_string);
+            conn.Open();
+            SqlCommand command = conn.CreateCommand();
+
+            Console.WriteLine($"\n\nNow enter {column_name} which you want to remove ");
+            Console.Write($"Enter {column_name} :");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            string query = $"delete {table_name} where {column_name}={id}";
+            command.CommandText = query;
+            command.ExecuteReader();
+
+            conn.Close();
+
+            Console.WriteLine($"\n\n{table_name} Removed Successfully..!!");
+            display_table(table_name);
+        }
+
+        
+        public void display_table(string table_name)
+        {
+            SqlConnection conn = new SqlConnection(conn_string);
+            conn.Open();
+            SqlCommand command = conn.CreateCommand();
+            string query = $"SELECT * FROM {table_name}";
+            command.CommandText = query;
+            SqlDataReader reader = command.ExecuteReader();
+            Console.WriteLine($"\nThis is your Current {table_name} Table\n");
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write(reader[i] + " ");
+                }
+                Console.WriteLine();
+            }
+            conn.Close();
+        }
+
+       
+    }
+}
---
+++ Database_Connectivity/Program.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections;
+using System.Data;
+
+namespace Database_Connectivity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string conn_string = "Data Source=DESKTOP-KN3OCS1;Initial Catalog=Practice_SQL;Integrated Security=True;Encrypt=False";
+            SqlConnection conn = new SqlConnection(conn_string);
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+
+            // ****************************SELECT & WHERE QUERIES****************************
+
+            //string query = "SELECT * FROM StudentDetails where age=22";
+            //cmd.CommandText = query;
+            //ArrayList lst=new ArrayList();
+            //SqlDataReader reader = cmd.ExecuteReader();
+            //while (reader.Read())
+            //{
+            //    Console.Write(reader.GetInt32(0) + "\t");
+            //    Console.Write(reader.GetString(1) + "\t");
+            //    Console.Write(reader.GetInt32(2) + "\t");
+            //    Console.Write(reader.GetDateTime(3).ToShortDateString()+ "\t");
+            //    Console.Write(reader.GetInt32(4) + "\t");
+            //    Console.Write(reader.GetString(5) + "\t");
+
+            //    Console.WriteLine();
+            //}
+            // ****************************SELECT & WHERE QUERIES without typecast****************************
+            //while (reader.Read())
+            //{
+            //    for (int i = 0; i < reader.FieldCount; i++)
+            //    {
+            //        Console.Write(reader[i] + " ");
+            //    }
+            //    Console.WriteLine();
+
+            //}
+            // **************************** Insert Command using Parameters ****************************
+            //string query = "INSERT INTO StudentDetails(studID,name,deptid,dob,age,email) VALUES(@id,@name,@deptid,@dob,@age,@email)";
+            //Console.Write("enter id"+" ");
+            //int id=Convert.ToInt32(Console.ReadLine());
+            //Console.Write("enter name" + " ");
+            //string name = Console.ReadLine();
+            //Console.Write("enter deptid" + " ");
+            //int deptid = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("enter dob" + " ");
+            //string dob = Console.ReadLine();
+            //Console.Write("enter age" + " ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("enter email" + " ");
+            //string email = Console.ReadLine();
+
+            //cmd.Parameters.AddWithValue("@id", id);
+            //cmd.Parameters.AddWithValue("@name", name);
+            //cmd.Parameters.AddWithValue("@deptid", deptid);
+            //cmd.Parameters.AddWithValue("@dob", dob);
+            //cmd.Parameters.AddWithValue("@age", age);
+            //cmd.Parameters.AddWithValue("@email", email);
+            //cmd.CommandText = query;
+            //cmd.ExecuteNonQuery();
+
+            // direct insert with value
+            //string query = "INSERT INTO StudentDetails(studID,name,deptid,dob,age,email) VALUES(200,'krithi',510,'2002-04-9',14,'krithi@gmail.com')";
+            //cmd.Parameters.AddWithValue("@id", "200");
+            //cmd.Parameters.AddWithValue("@name", "krithi");
+            //cmd.Parameters.AddWithValue("@deptid", "510");
+            //cmd.Parameters.AddWithValue("@dob", "2002-04-20");
+            //cmd.Parameters.AddWithValue("@age", "14");
+            //cmd.Parameters.AddWithValue("@email", "krithi@gmail.com");
+            //cmd.CommandText = query;
+            //cmd.ExecuteNonQuery();
+            // deleting
+            //try
+            //{
+            //    Console.Write("enter id" + " ");
+            //    int id = Convert.ToInt32(Console.ReadLine());
+
+            //    string query = $"delete from StudentDetails where studIDd={id}";
+            //    cmd.CommandText = query;
+            //    cmd.ExecuteReader();
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message.ToString());
+            //}
+
+            //Console.Write("enter dept id to which we have to change to null ");
+            //int deptid=Convert.ToInt32(Console.ReadLine());
+            //string query = $"update StudentDetails set deptid=000 where deptid={deptid}";
+            //cmd.CommandText = query;
+            //cmd.ExecuteReader();
+
+            //string query = "exec callInsertProcedure 999,'shannn',502";
+            //int id = 999;
+            //int deptid = 501;
+            //string name = "shann";
+            //using(SqlCommand command=new SqlCommand("callInsertProcedure", conn))
+            //{
+            //    command.CommandType = System.Data.CommandType.StoredProcedure;
+            //    command.Parameters.Add("id", SqlDbType.Int).Value = 909;
+            //    command.Parameters.Add("name", SqlDbType.VarChar).Value = "priyaaaa";
+            //    command.Parameters.Add("dept", SqlDbType.Int).Value = 505;
+            //    command.ExecuteReader();
+            //}
+
+
+            //using (SqlCommand command = conn.CreateCommand())
+            //{
+            //    command.CommandText = "select * from dbo.fetchStudent(@dept_id)";
+            //    command.Parameters.Add(new SqlParameter("@dept_id", 504));
+            //    SqlDataReader rd = command.ExecuteReader();
+            //    while (rd.Read())
+            //    {
+            //        Console.WriteLine(rd[0]);
+            //    }
+            //}
+
+
+            conn.Close();
+
+        }
+    }
+}
+
+//ananth code
+
+//using (SqlCommand cmd = conn.CreateCommand())
+//{
+//    cmd.CommandText = "select * from dbo.GetDobById(@name)";
+
+//    cmd.Parameters.Add(new SqlParameter("@name", "John Doe"));
+
+//    SqlDataReader reader = cmd.ExecuteReader();
+
+//    DataTable dt = new DataTable();
+//    dt.Load(reader);
+//    Console.Write($" {dt.Rows[0][0]} - {dt.Rows[0][1]} - {dt.Rows[0][2]} ");
+
+//}
