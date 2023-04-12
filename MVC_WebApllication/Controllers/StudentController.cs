9880ea1b0ea0537f858ea659c7679b89aa01b5cf
priya-shan <shanmugapriyashanu2002@gmail.com>
2023-04-12 14:49:00 +0530

    data from 2 tables using 2 conn string

--- MVC_WebApllication/Controllers/StudentController.cs
+++ MVC_WebApllication/Controllers/StudentController.cs
@@ -19,6 +19,7 @@ namespace MVC_WebApllication.Controllers
             //string conn_string = "Data Source=DESKTOP-KN3OCS1;Initial Catalog=Practice_SQL;Integrated Security=True;Encrypt=False";
             //SqlConnection conn = new SqlConnection(conn_string);
             string conn_string = configuration.GetConnectionString("StudentDB");
+            string conn_string2= configuration.GetConnectionString("libraryDB");
             SqlConnection conn = new SqlConnection();
             conn.ConnectionString = conn_string;
             conn.Open();
@@ -33,6 +34,24 @@ namespace MVC_WebApllication.Controllers
                 sm.id = reader.GetInt32(0);
                 sm.name = reader.GetString(1);
                 sm.dept = reader.GetInt32(2);
+                student_lst.Add(sm);
+            }
+            conn.Close();
+
+            SqlConnection conn2 = new SqlConnection();
+            conn.ConnectionString = conn_string2;
+            conn.Open();
+            SqlCommand cmd2 = conn.CreateCommand();
+            string query2 = "select * from lms_book_details";
+            cmd.CommandText = query2;
+            SqlDataReader reader2 = cmd.ExecuteReader();
+
+            while (reader2.Read())
+            {
+                StudentModel sm = new StudentModel();
+                sm.id = 0;
+                sm.name = reader2.GetString(1);
+                sm.dept =0;
                 student_lst.Add(sm);
             }
             conn.Close();
