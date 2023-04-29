157e9d4880f623c37abc40dc03265387eff6ce49
priya-shan <shanmugapriyashanu2002@gmail.com>
2023-04-29 13:00:04 +0530

    updated

--- MVC_WebApplication_Sample/Controllers/BookDetailsController.cs
+++ MVC_WebApplication_Sample/Controllers/BookDetailsController.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
-using MVC_WebApplication_Sample.Models;
+using Newtonsoft.Json;
 using System.Data;
+using System.Net.Http.Headers;
 
 namespace MVC_WebApplication_Sample.Controllers
 {
@@ -15,171 +16,278 @@ namespace MVC_WebApplication_Sample.Controllers
             _configuration = configuration;
             conn = new SqlConnection(_configuration.GetConnectionString("libraryDB"));
         }
-         
-        public List<BookModel> GetBooks()
-        {
-            List<BookModel> book_list = new List<BookModel>();
-            conn.Open();
-            SqlCommand cmd = new SqlCommand("fetch_book_details", conn);
-            cmd.CommandType = CommandType.StoredProcedure;
-            SqlDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                BookModel book = new BookModel();
-                book.book_code = (int)reader["book_code"];
-                book.book_title = (string)reader["book_title"];
-                book.category = (string)reader["category"];
-                book.author = (string)reader["author"];
-                book_list.Add(book);
-            }
-            reader.Close();
-            conn.Close();
-            return book_list;
-        }
 
-        public BookModel getMovieWithId(int id)
-        {
-            conn.Open();
-            SqlCommand cmd = new SqlCommand("fetch_book_withId", conn);
-            cmd.CommandType = CommandType.StoredProcedure;
+        //public List<BookModel> GetBooks()
+        //{
+        //    List<BookModel> book_list = new List<BookModel>();
+        //    conn.Open();
+        //    SqlCommand cmd = new SqlCommand("fetch_book_details", conn);
+        //    cmd.CommandType = CommandType.StoredProcedure;
+        //    SqlDataReader reader = cmd.ExecuteReader();
+        //    while (reader.Read())
+        //    {
+        //        BookModel book = new BookModel();
+        //        //book.book_code = Convert.ToInt32(reader["book_code"]);
+        //        book.book_title = (string)reader["book_title"];
+        //        book.category = (string)reader["category"];
+        //        book.author = (string)reader["author"];
+        //        book_list.Add(book);
+        //    }
+        //    reader.Close();
+        //    conn.Close();
+        //    return book_list;
+        //}
 
-            cmd.Parameters.AddWithValue("@id", id);
+        //public BookModel getMovieWithId(int id)
+        //{
+        //    conn.Open();
+        //    SqlCommand cmd = new SqlCommand("fetch_book_withId", conn);
+        //    cmd.CommandType = CommandType.StoredProcedure;
 
-            SqlDataReader reader = cmd.ExecuteReader();
+        //    cmd.Parameters.AddWithValue("@id", id);
 
-            BookModel book = new();
+        //    SqlDataReader reader = cmd.ExecuteReader();
 
-            while (reader.Read())
-            {
-                book.book_code = (int)reader["book_code"];
-                book.book_title = (string)reader["book_title"];
-                book.category = (string)reader["category"];
-                book.author = (string)reader["author"];
-            }
-            return book;
-        }
-        void InsertBook(BookModel Book)
-        {
-            conn.Open();
-            SqlCommand cmd = new SqlCommand("add_book", conn);
-            cmd.CommandType = CommandType.StoredProcedure;
+        //    BookModel book = new();
 
-            cmd.Parameters.AddWithValue("@book_code",Book.book_code );
-            cmd.Parameters.AddWithValue("@book_title",Book.book_title);
-            cmd.Parameters.AddWithValue("@category", Book.category);
-            cmd.Parameters.AddWithValue("@author",Book.author);
+        //    while (reader.Read())
+        //    {
+        //        //book.book_code = (int)reader["book_code"];
+        //        book.book_title = (string)reader["book_title"];
+        //        book.category = (string)reader["category"];
+        //        book.author = (string)reader["author"];
+        //    }
+        //    return book;
+        //}
+        //void InsertBook(BookModel Book)
+        //{
+        //    conn.Open();
+        //    SqlCommand cmd = new SqlCommand("add_book", conn);
+        //    cmd.CommandType = CommandType.StoredProcedure;
 
-            cmd.ExecuteNonQuery();
+        //    cmd.Parameters.AddWithValue("@book_code",Book.book_code );
+        //    cmd.Parameters.AddWithValue("@book_title",Book.book_title);
+        //    cmd.Parameters.AddWithValue("@category", Book.category);
+        //    cmd.Parameters.AddWithValue("@author",Book.author);
 
-            conn.Close();
+        //    cmd.ExecuteNonQuery();
 
-        }
-        public void UpdateBook(int id,BookModel Book)
-        {
-            conn.Open();
-            SqlCommand cmd = new SqlCommand("edit_book_withID", conn);
-            cmd.CommandType = CommandType.StoredProcedure;
+        //    conn.Close();
 
-            cmd.Parameters.AddWithValue("@book_code", Book.book_code);
-            cmd.Parameters.AddWithValue("@book_title", Book.book_title);
-            cmd.Parameters.AddWithValue("@category", Book.category);
-            cmd.Parameters.AddWithValue("@author", Book.author);
+        //}
+        //public void UpdateBook(int id,BookModel Book)
+        //{
+        //    conn.Open();
+        //    SqlCommand cmd = new SqlCommand("edit_book_withID", conn);
+        //    cmd.CommandType = CommandType.StoredProcedure;
 
-            cmd.ExecuteNonQuery();
+        //    cmd.Parameters.AddWithValue("@book_code", Book.book_code);
+        //    cmd.Parameters.AddWithValue("@book_title", Book.book_title);
+        //    cmd.Parameters.AddWithValue("@category", Book.category);
+        //    cmd.Parameters.AddWithValue("@author", Book.author);
 
-            conn.Close();
-        }
+        //    cmd.ExecuteNonQuery();
+
+        //    conn.Close();
+        //}
         // GET: BookDetailsController
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View(GetBooks());
+            string Baseurl = "https://localhost:7259/";
+            List<BookModel1> EmpInfo = new List<BookModel1>();
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient
+                HttpResponseMessage Res = await client.GetAsync("api/Book/");
+                //Checking the response is successful or not which is sent using HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+                    Console.Write("res  " + EmpResponse);
+                    //Deserializing the response recieved from web api and storing into the Employee list
+                    EmpInfo = JsonConvert.DeserializeObject<List<BookModel1>>(EmpResponse);
+                }
+                foreach (var item in EmpInfo)
+                {
+                }
+                //returning the employee list to view
+                //return View(EmpInfo);
+
+                return View(EmpInfo);
+            }
         }
 
         // GET: BookDetailsController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View(getMovieWithId(id));
-        }
-
-        // GET: BookDetailsController/Create
-        public ActionResult Create()
+        public async Task<ActionResult> details(int id)
         {
 
-            return View();
-        }
-
-        // POST: BookDetailsController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(BookModel Book)
-        {
-            try
+            string Baseurl = "https://localhost:7259/";
+            BookModel1 model = new BookModel1();
+            using (var client = new HttpClient())
             {
-                InsertBook(Book);
-                return RedirectToAction(nameof(Index));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return View();
-            }
-        }
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient
+                HttpResponseMessage Res = await client.GetAsync($"api/Book/{id}");
+                //Checking the response is successful or not which is sent using HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+                    Console.Write("res  " + EmpResponse);
+                    //Deserializing the response recieved from web api and storing into the Employee list
+                    model = JsonConvert.DeserializeObject<BookModel1>(EmpResponse);
+                }
+                //returning the employee list to view
+                //return View(EmpInfo);
 
-        // GET: BookDetailsController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View(getMovieWithId(id));
-        }
-
-        // POST: BookDetailsController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, BookModel Book)
-        {
-            try
-            {
-                Console.WriteLine("edit post");
-                UpdateBook(id, Book);
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
+                return View(model);
             }
         }
 
-        // GET: BookDetailsController/Delete/5
-        public ActionResult Delete(int id)
+        //// GET: BookDetailsController/Create
+        //public ActionResult Create()
+        //{
+
+        //    return View();
+        //}
+
+        //// POST: BookDetailsController/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(BookModel Book)
+        //{
+        //    try
+        //    {
+        //        InsertBook(Book);
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.ToString());
+        //        return View();
+        //    }
+        //}
+
+        //// GET: BookDetailsController/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View(getMovieWithId(id));
+        //}
+
+        //// POST: BookDetailsController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, BookModel Book)
+        //{
+        //    try
+        //    {
+        //        Console.WriteLine("edit post");
+        //        UpdateBook(id, Book);
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: BookDetailsController/Delete/5
+        public async Task<IActionResult> Delete(int id)
         {
-            return View(getMovieWithId(id));
+            string Baseurl = "https://localhost:7259/";
+            BookModel1 model = new BookModel1();
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient
+                HttpResponseMessage Res = await client.GetAsync($"api/Book/{id}");
+                //Checking the response is successful or not which is sent using HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+                    Console.Write("res  " + EmpResponse);
+                    //Deserializing the response recieved from web api and storing into the Employee list
+                    model = JsonConvert.DeserializeObject<BookModel1>(EmpResponse);
+                }
+                //returning the employee list to view
+                //return View(EmpInfo);
+
+                return View(model);
+            }
         }
 
-        public void deleteWithId(int id)
-        {
-            conn.Open();
-            SqlCommand cmd = new SqlCommand("delete_book_withId", conn);
-            cmd.CommandType = CommandType.StoredProcedure;
+        //public void deleteWithId(int id)
+        //{
+        //    conn.Open();
+        //    SqlCommand cmd = new SqlCommand("delete_book_withId", conn);
+        //    cmd.CommandType = CommandType.StoredProcedure;
 
-            cmd.Parameters.AddWithValue("@id", id);
+        //    cmd.Parameters.AddWithValue("@id", id);
 
-            cmd.ExecuteNonQuery();
+        //    cmd.ExecuteNonQuery();
 
-            conn.Close();
-        }
+        //    conn.Close();
+        //}
+
         // POST: BookDetailsController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<ActionResult> Delete(int id, IFormCollection collection)
         {
-            try
+            string Baseurl = "https://localhost:7259/";
+            BookModel1 model = new BookModel1();
+            using (var client = new HttpClient())
             {
-                deleteWithId(id);
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient
+                HttpResponseMessage Res = await client.DeleteAsync($"api/Book/{id}");
+                //Checking the response is successful or not which is sent using HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+                    Console.Write("res  " + EmpResponse);
+                    //Deserializing the response recieved from web api and storing into the Employee list
+                    model = JsonConvert.DeserializeObject<BookModel1>(EmpResponse);
+                }
+                //returning the employee list to view
+                //return View(EmpInfo);
+
+                return RedirectToAction("Index");
             }
         }
     }
-}
+        public class BookModel1
+        {
+            public string book_code { get; set; }
+
+            public string book_title { get; set; }
+            public string category { get; set; }
+            public string author { get; set; }
+            public string publication { get; set; }
+
+            public double price { get; set; }
+            public int rem_quantity { get; set; }
+
+            public int sold_quantity { get; set; }
+        }
+    }
--- MVC_WebApplication_Sample/Controllers/HomeController.cs
+++ MVC_WebApplication_Sample/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVC_WebApplication_Sample.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Diagnostics;
+using System.Net.Http.Headers;
 
 namespace MVC_WebApplication_Sample.Controllers
 {
@@ -13,14 +16,78 @@ namespace MVC_WebApplication_Sample.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            string Baseurl = "https://localhost:7259/";
+            List<BookModel> EmpInfo = new List<BookModel>();
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient
+                HttpResponseMessage Res = await client.GetAsync("api/Book");
+                //Checking the response is successful or not which is sent using HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+                    Console.Write("res  " + EmpResponse);
+                    //Deserializing the response recieved from web api and storing into the Employee list
+                    EmpInfo = JsonConvert.DeserializeObject<List<BookModel>>(EmpResponse);
+                }
+                foreach(var item in EmpInfo)
+                {
+                    Console.WriteLine(item.author);
+                }
+                //returning the employee list to view
+                //return View(EmpInfo);
+
+                return View();
+            }
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> Privacy()
         {
-            return View();
+            BookModel model = new BookModel();
+            model.book_code = "a";
+            model.book_title = "title";
+            model.category = "cat";
+            model.author = "auth-priya";
+            model.publication = "pub";
+            model.price = 123;
+            model.rem_quantity = 0;
+            model.sold_quantity = 10;
+
+            string Baseurl = "https://localhost:7259/";
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //var json = JsonConvert.SerializeObject(model, Formatting.Indented);
+
+                //var stringContent = new StringContent(json);
+
+                var Res = await client.PostAsJsonAsync<BookModel>("api/Book", model);
+                if (Res.IsSuccessStatusCode)
+                {
+
+                    Console.Write("res  success");
+
+                }
+                else
+                {
+                    Console.Write("res  failed");
+                }
+                //returning the employee list to view
+                //return View(EmpInfo);
+
+                return View();
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -28,5 +95,20 @@ namespace MVC_WebApplication_Sample.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+    }
+
+    public class BookModel
+    {
+        public string book_code { get; set; }
+
+        public string book_title { get; set; }
+        public string category { get; set; }
+        public string author { get; set; }
+        public string publication { get; set; }
+
+        public double price { get; set; }
+        public int rem_quantity { get; set; }
+
+        public int sold_quantity { get; set; }
     }
 }
